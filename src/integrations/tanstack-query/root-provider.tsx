11c2365154bb72bde163aed87973335888146e76
Mathieu <ma.scarlatella@gmail.com>
2026-05-19 20:45:03 -0400

    fix: update url method

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,3 +1,5 @@
+import { TRPCProvider } from "#/integrations/trpc/react";
+import type { TRPCRouter } from "#/integrations/trpc/router";
 import { QueryClient } from "@tanstack/react-query";
 import { createIsomorphicFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
@@ -5,18 +7,18 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import type { ReactNode } from "react";
 import superjson from "superjson";
-import { TRPCProvider } from "#/integrations/trpc/react";
-import type { TRPCRouter } from "#/integrations/trpc/router";
 
 function getUrl() {
 	if (typeof window !== "undefined") return "/api/trpc";
 
-	if (process.env.VERCEL_URL) {
-		return `https://${process.env.VERCEL_URL}/api/trpc`;
-	}
 	if (process.env.APP_URL) {
 		return `${process.env.APP_URL.replace(/\/$/, "")}/api/trpc`;
 	}
+
+	if (process.env.VERCEL_URL) {
+		return `https://${process.env.VERCEL_URL}/api/trpc`;
+	}
+
 	return `http://localhost:${process.env.PORT ?? 3000}/api/trpc`;
 }
 
